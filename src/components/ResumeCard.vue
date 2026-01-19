957f8668b2ad8b64f6f48881788e12ba9879b0c2
Jonnytoshen <jonnytoshen@gmail.com>
2026-01-19 16:22:51 +0800

    feat: add ResumeCard component with title prop and slot support

---
+++ src/components/ResumeCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string
+}>()
+</script>
+
+<template>
+  <div class="p-4 border border-slate-200 rounded-2xl">
+    <div class="mb-4" v-if="props.title">
+      <h3 class="font-bold text-gray-800">{{ props.title }}</h3>
+    </div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
