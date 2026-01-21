2f15e0f009c5c237b0c57aaad7332b62416e8713
Jonnytoshen <jonnytoshen@gmail.com>
2026-01-21 14:26:14 +0800

    feat: implement BackgroundDecor component for enhanced visual styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BackgroundDecor from './components/BackgroundDecor.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main
+    class="lg:rounded-6xl lg:shadow-root relative mx-auto max-w-5xl overflow-hidden bg-white lg:my-16"
+  >
+    <BackgroundDecor />
+    <div class="relative z-10">
+      <RouterView />
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/BackgroundDecor.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="absolute inset-0 z-0">
+    <div
+      class="blur-4xl absolute top-0 left-0 aspect-square w-6/10 -translate-y-1/2 transform rounded-full bg-indigo-600/15"
+    ></div>
+    <div
+      class="absolute bottom-0 left-1/2 aspect-2/1 w-8/10 -translate-x-1/2 translate-y-1/2 transform rounded-full bg-indigo-600/10 blur-3xl"
+    ></div>
+  </div>
+</template>
