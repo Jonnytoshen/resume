82103c443c93d57d589fae1e55daebfacab3ba1f
Jonnytoshen <jonnytoshen@gmail.com>
2026-01-21 14:43:02 +0800

    feat: add SectionTitle component

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue'
+
+const attrs = useAttrs()
+</script>
+
+<template>
+  <div v-bind="attrs" class="flex items-center gap-4">
+    <div
+      class="flex size-9 items-center justify-center rounded-lg bg-indigo-50 text-indigo-600"
+      v-if="$slots.icon"
+    >
+      <slot name="icon" />
+    </div>
+    <h2 class="text-lg font-bold text-gray-800">
+      <slot />
+    </h2>
+  </div>
+</template>
