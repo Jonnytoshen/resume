7dbbade7c443152bdb8897145b6d81b09b9e8ea1
Jonnytoshen <jonnytoshen@gmail.com>
2026-01-21 14:32:05 +0800

    style: reorder class attributes for improved readability in ResumeCard component

--- src/components/ResumeCard.vue
+++ src/components/ResumeCard.vue
@@ -5,7 +5,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="p-4 border border-slate-200 rounded-2xl">
+  <div class="rounded-2xl border border-slate-200 p-4">
     <div class="mb-4" v-if="props.title">
       <h3 class="font-bold text-gray-800">{{ props.title }}</h3>
     </div>
