2e8a25a19f0eb49bd7e52bbf2a7b9b1229c0c054
Jonnytoshen <jonnytoshen@gmail.com>
2026-01-22 14:38:52 +0800

    chore: add commit-msg hook with commitlint to lint commit msg

---
+++ commitlint.config.ts
@@ -0,0 +1,9 @@
+/** @type {import("@commitlint/types").UserConfig} */
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+  },
+}
