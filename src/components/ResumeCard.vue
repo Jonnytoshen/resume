060a508c1a1bb14f7dc3fd537765a44fdaf75b35
Jonnytoshen <jonnytoshen@gmail.com>
2026-01-22 10:36:33 +0800

    feat: enhance ResumeCard layout to support additional slot content

--- src/components/ResumeCard.vue
+++ src/components/ResumeCard.vue
@@ -6,8 +6,13 @@ const props = defineProps<{
 
 <template>
   <div class="rounded-2xl border border-slate-200 p-4">
-    <div class="mb-4" v-if="props.title">
-      <h3 class="font-bold text-gray-800">{{ props.title }}</h3>
+    <div class="mb-4 flex items-center justify-between gap-4" v-if="props.title || $slots.extra">
+      <div class="flex-auto" v-if="props.title">
+        <h3 class="font-bold text-gray-800">{{ props.title }}</h3>
+      </div>
+      <div class="flex-none" v-if="$slots.extra">
+        <slot name="extra" />
+      </div>
     </div>
     <div>
       <slot />
